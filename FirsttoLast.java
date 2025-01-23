9f9fe57f299dc99aa8ac9d4986df7becb539d919
Anou Zecharia W B <anouworld@gmail.com>
2025-01-23 07:30:04 +0530

    Add files via upload

---
+++ FirsttoLast.java
@@ -0,0 +1,13 @@
+public class FirsttoLast 
+{
+	public static void main(String args[])
+	{
+		int first = 2;
+		int last = 7;
+		while(first<=last)
+		{
+		System.out.print(first);
+		first=first+1;		
+		}
+	}
+}
---
+++ lasttofirst.java
@@ -0,0 +1,13 @@
+public class lasttofirst
+{
+	public static void main(String args[])
+	{
+		int first=2;
+		int last=7;
+		while(last>=first)
+		{
+		System.out.println(last);
+		last--;
+		}
+	}
+}
